6a5168ebf532da4b0667b5ef0162f474eaa71102
Hillal <hillal@Hillals-MacBook-Air.local>
2024-05-28 23:20:26 +0300

    Create the audit logs table .

---
+++ database/migrations/2024_05_28_131941_audit_log_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        schema::create('audit_logs', function (Blueprint $table) {
+            $table->id();
+            $table->unSignedBigInteger('user_id');
+            $table->string('action')->comment('The action that was performed .i.e GET, POST, PATCH, DELETE');
+            $table->string('path');
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnUpdate()->restictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('audit_logs');
+    }
+};
