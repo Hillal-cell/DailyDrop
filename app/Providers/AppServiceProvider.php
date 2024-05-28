fc97ba616af618fa30a7299ae361c55f23f85c2f
Hillal <hillal@Hillals-MacBook-Air.local>
2024-05-28 23:23:55 +0300

    Registered the Audit log middle ware for the function boot.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Http\Middleware\LogAudit;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        $this->app['router']->aliasMiddleware('log.audit', LogAudit::class);
     }
 }
