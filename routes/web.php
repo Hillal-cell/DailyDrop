9916128d88f2863d7fb2baedb43e476b6729583a
Hillal <hillal@Hillals-MacBook-Air.local>
2024-05-28 23:21:21 +0300

    Added the middleware for the audit logs to the varous middleware groups.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Models\EventTable;
+use App\Http\Middleware\LogAudit;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,9 +17,9 @@ Route::get('/dashboard', function () {
         
      // Pass the channels to the view
      return view('dashboard', ['channels' => $channels]);
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified','log.audit'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth','log.audit')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -27,11 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/get-cast/{castName}', [ProfileController::class, 'getCast'])->name('profile.getCast');
     Route::get('/channel/{channelName}/calendar', [ProfileController::class, 'showChannelCalendar'])->name('channel.calendar');
     Route::get('/channel/{channelName}/events', [ProfileController::class, 'getChannelEvents']);
-
-
-    //try to change it to post and see
-    
-    Route::PUT('/update-event/{castName}', [ProfileController::class, 'updateEvent'])->name('profile.updateEvent');
+    Route::get('/report',[ProfileController::class,'getReport'])->name('report');
+    Route::put('/update-event/{castName}', [ProfileController::class, 'updateEvent'])->name('profile.updateEvent');
 
 
     
