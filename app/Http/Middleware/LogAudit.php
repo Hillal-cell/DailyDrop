a0601507377485262cd4e39d4000ff3236debbe3
Hillal <hillal@Hillals-MacBook-Air.local>
2024-05-28 23:18:44 +0300

    Added the Middleware directory to hold the LogAudit.

---
+++ app/Http/Middleware/LogAudit.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\AuditLog;
+use Illuminate\Support\Facades\Auth;
+
+
+class LogAudit
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $auditLog = new AuditLog();
+        $auditLog->user_id = Auth::id();
+        $auditLog->action = $request->method();
+        $auditLog->path = $request->path();
+        $auditLog->save();
+
+        return $next($request);
+    }
+}
