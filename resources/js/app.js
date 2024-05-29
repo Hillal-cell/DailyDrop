aaeac9e217544291fb7706a9b52eff0cbc190280
Hillal <sserunjogi.hillal@students.mak.ac.ug>
2024-05-29 15:28:25 +0300

    Added event listebners to the cancle and close buttons .This is incase a user wants to cancel the pop up.

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,6 +16,12 @@ Alpine.start();
 
 document.addEventListener("DOMContentLoaded", function () {
 
+
+     document.addEventListener("dblclick", function (event) {
+         // console.log("Clicked element:", event.target);
+         handleCastNameClick(event);
+     });
+
     const calendarEl = document.getElementById("calendar");
     const calendar = new Calendar(calendarEl, {
         plugins: [dayGridPlugin, timeGridPlugin, listPlugin, multiMonthPlugin],
@@ -49,11 +55,29 @@ document.addEventListener("DOMContentLoaded", function () {
     if (saveEventButton) {
         saveEventButton.removeEventListener("click", saveEvent);
         saveEventButton.addEventListener("click", saveEvent);
+    }else{
+        console.log("saveEventButton not found");
     }
 
     const updateEventButton = document.getElementById("update_event_button");
     if (updateEventButton) {
         updateEventButton.removeEventListener("click", handleCastNameClick);
         updateEventButton.addEventListener("click", handleCastNameClick);
+    }else{
+        console.log("updateEventButton not found");
     }
+
+    const dismissModalButton = document.getElementById("dismiss_modal_button");
+    if (dismissModalButton) {
+        dismissModalButton.addEventListener("click", function () {
+            $("#event_entry_modal").modal("hide");
+            location.reload();
+        });}
+    const dismissModalButton1 = document.getElementById("dismiss_modal_button1");
+    if (dismissModalButton1) {
+        dismissModalButton1.addEventListener("click", function () {
+            $("#event_entry_modal").modal("hide");
+            location.reload();
+        });}
+
 });
