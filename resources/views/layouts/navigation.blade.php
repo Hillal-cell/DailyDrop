b35609b4c5a1ef1318e9f952b27b9e4790db6d17
Hillal <sserunjogi.hillal@students.mak.ac.ug>
2024-05-29 20:56:00 +0300

    Modified the navigation blade.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,12 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('report')" :active="request()->routeIs('report')">
+                        {{ __('Report') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -23,7 +29,7 @@
                     <x-dropdown align="left" width="48">
                         <x-slot name="trigger">
                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                                <div><span style="color: gold">Hello</span> {{ Auth::user()->name }}</div>
+                                <div><span style="color: blueviolet">Hello</span> {{ Auth::user()->name }}</div>
 
                                 <div class="ms-1">
                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
