ebbcdb7d95d8fc7463d31ed32fca9cdfb8a0a628
Hillal <sserunjogi.hillal@students.mak.ac.ug>
2024-06-20 22:13:11 +0300

    Blade to enable dit of user info.

---
+++ resources/views/editUser.blade.php
@@ -0,0 +1,73 @@
+<x-app-layout>
+
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-red-800 leading-tight">
+            {{ __('User edit pannel') }}
+        </h2>
+        </h2>
+    </x-slot>
+
+
+    
+    @if (session('status'))
+        <div class="alert alert-success" id="status-message" style="text-align:center;">
+            {{ session('status') }}
+        </div>
+    @elseif (session('error'))
+        <div class="alert alert-danger" id="status-message" style="text-align:center;">
+            {{ session('error') }}
+        </div>
+    @endif
+    @if ($errors->any())
+        <div class="alert alert-danger" id="status-message" style="text-align:center;">
+            @foreach ($errors->all() as $error)
+                {{ $error }}
+            @endforeach
+        </div>
+    @endif
+    
+    
+     <!-- Form to edit the data -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <form action="{{ route('updateUser', $user->id) }}" method="post">
+                    @csrf
+                    @method('PATCH')
+                    <div class="form-group">
+                        <label for="name">Name:</label>
+                        <input type="text" class="form-control" name="name" value="{{ $user->name }}" />
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email:</label>
+                        <input type="text" class="form-control" name="email" value="{{ $user->email }}" />
+                    </div>
+                    <div class="form-group">
+                        <label for="role">Role:</label>
+                        <select name="role" class="form-control">
+                            <option value="user" {{ $user->role == 'user' ? 'selected' : '' }}>User</option>
+                            <option value="admin" {{ $user->role == 'admin' ? 'selected' : '' }}>Admin</option>
+                        </select>
+                    </div>
+
+                    
+                        <button type="submit" class="btn btn-primary">Update</button>
+                        <button type="button" class="btn btn-danger" onclick="window.location.href='{{ route('getUsers') }}'">Cancel</button>
+                    
+                    
+                </form>
+
+    <!-- script to hide the status message after 5 seconds -->
+    <script>
+        window.onload = function() {
+            setTimeout(function() {
+                var statusMessage = document.getElementById('status-message');
+                if (statusMessage) statusMessage.style.display = 'none';
+            }, 2000);
+        }
+    </script>
+
+</x-app-layout>
+
+
