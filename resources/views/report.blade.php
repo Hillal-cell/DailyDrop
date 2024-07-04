83fbee25e5915cd24f6a6db3fe22a593455615bb
Hillal <sserunjogi.hillal@students.mak.ac.ug>
2024-07-04 15:12:16 +0300

    Added the reporting page.

---
+++ resources/views/report.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Reporting') }}
+        </h2>
+    </x-slot>
+
+
+    <div>
+        <?php
+            echo "<h1> Welcome to the reporting page!.</h1>"
+        ?>
+    </div>
+
+</x-app-layout>
+
+
