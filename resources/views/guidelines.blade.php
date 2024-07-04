337b10e4d4328ab621559a385f8bad508d0dd3d1
Hillal <sserunjogi.hillal@students.mak.ac.ug>
2024-07-04 15:15:06 +0300

    Added the guidelines page.

---
+++ resources/views/guidelines.blade.php
@@ -0,0 +1,17 @@
+ <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Guidelines') }}
+        </h2>
+    </x-slot>
+
+
+    <div>
+        <?php
+            echo "<h1>This is the guidelines page!.</h1>"
+        ?>
+    </div>
+
+</x-app-layout>
+
+
